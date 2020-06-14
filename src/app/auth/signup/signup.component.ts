222b6ed600d44f68bfb33ff37b7f6e422a4b8cfc
George <gheorghita.harpa0423@yahoo.com>
2020-06-14 23:57:51 +0100

    Implement User Registration in Angular Application Part 1.

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+}
